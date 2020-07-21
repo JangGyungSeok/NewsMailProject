6fd27bb1d5e9d3e4a53fd1d52dd923b3b5608cbe
JKS <juskus0310@naver.com>
2020-07-21 10:50:27 +0900

    크롤링 로직 수정 ( 현재일자의 기사만을 크롤링, 10개이하의 기사만 크롤링 ) GatewayController 생성 ( 유입 사용자 확인 후 기사 url로 redirect하는 로직 작성예정)

--- Practice/app/Http/Controllers/CrawlerController.php
+++ Practice/app/Http/Controllers/CrawlerController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use \Snoopy;
 use Goutte\Client;
 use App\NewsData;
+use Illuminate\Support\Facades\Log;
+
 class CrawlerController extends Controller
 {
     protected $news_data;
@@ -16,7 +18,7 @@ class CrawlerController extends Controller
     }
 
 
-    public function crawlingNews(NewsData $news){
+    public function crawlingNews(){
         $client = new Client();
         // 'http://www.hkrecruit.co.kr/'
         // $crawler = $client->submit(
@@ -24,38 +26,58 @@ class CrawlerController extends Controller
         //     array('sc_word'=>'사람인')
         // );
 
-        $isToday = true;
-        $page =1;
+        $page = 1;
+        $this->count = 0;
         // $isToday 로 변경할 예정
-        while($page <4){
+        while($this->count < 10){
             $url = 'http://www.hkrecruit.co.kr/news/articleList.html?page='.(string)$page.'&total=593&box_idxno=&sc_area=A&view_type=sm&sc_word=%EC%82%AC%EB%9E%8C%EC%9D%B8';
             // print($url);
             $crawler = $client->request('GET',$url);
             $page++;
-            $links = $crawler->filter('#user-container > div.float-center.max-width-1080 > div.user-content > section > article > div.article-list > section > div.list-block')->each(function($node){
+
+
+            $links = $crawler->filter('#user-container > div.float-center.max-width-1080 > div.user-content > section > article > div.article-list > section > div.list-block')
+                ->each(function ($node, $page) {
             // 날짜확인 로직 시작
-            $newsDate = explode("|", $node->filter('div.list-dated')->text())[2];
-            $newsDate = explode(' ', $newsDate)[1];
+                    if ($this->count >=10){
+                        return true;
+                    }
 
-            // 오늘 날짜와 비교하는 조건문
-            // date("Y-m-d") -> 서버시간을 기준으로 문자열 생성
-            // if(date("Y-m-d") == $newsDate){
-            return array($newsDate
-                ,$node->filter('div.list-titles > a > strong')->text()
-                ,'http://www.hkrecruit.co.kr'.$node->filter('div.list-titles > a')->attr('href'),
-                );
-            // }else{
-            //     $isToday = false;
+                    $newsDate = explode("|", $node->filter('div.list-dated')->text())[2];
+                    $newsDate = explode(' ', $newsDate)[1];
+
+                    // 오늘 날짜와 비교하는 조건문
+                    // date("Y-m-d") -> 서버시간을 기준으로 문자열 생성
+                    if(date("Y-m-d") == $newsDate){
+                        Log::info('newsData insert 메서드 실행!');
+                        $this->count++;
+                        $this->news_data->insertNews(
+                            array(
+                                $newsDate
+                                ,$node->filter('div.list-titles > a > strong')->text()
+                                ,'http://www.hkrecruit.co.kr'.$node->filter('div.list-titles > a')->attr('href'),
+                            )
+                        );
+                            // return array($newsDate
+                        //     ,$node->filter('div.list-titles > a > strong')->text()
+                        //     ,'http://www.hkrecruit.co.kr'.$node->filter('div.list-titles > a')->attr('href'),
+                        //     );
+                    } else {
+                        if ($this->count > 0) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                // 날짜확인 로직 끝
+
+                },['page' => $page]);
+
+
+            // foreach($links as $link){
+            //     // $this->news_data->insertNews($link);
+            //     print($link[0]);
             // }
-            // 날짜확인 로직 끝
-
-            });
-
-
-            foreach($links as $link){
-                // $this->news_data->insertNews($link);
-                print($link[0]);
-            }
 
         }
 
--- Practice/app/Http/Controllers/GatewayController.php
+++ Practice/app/Http/Controllers/GatewayController.php
@@ -7,10 +7,14 @@ use Illuminate\Support\Facades\Log;
 
 class GatewayController extends Controller
 {
+    // 요청 정보를 확인할 수 있는 Request객체를 사용
     public function enterGateway(Request $request){
 
 
-        Log::info($request -> query('url'));
+        // Log::info($request -> query('url'));
+        // 수신자 확인 로직 실행
+        // 수신자별 접근시간 적재로직 실행
+
         return Redirect($request -> query('url'));
     }
 }
